6130b0eb55046b59092195209b4cd91493656266
AST <elastraepicgames@gmail.com>
2026-06-25 20:00:16 +0200

    Update .lua

--- .lua
+++ .lua
@@ -1,1 +1,5 @@
-
+local s = {
+    ["14939963714"] = "loadstring(game:HttpGet("https://raw.githubusercontent.com/astral-123/jailbird/refs/heads/main/.lua"))()",
+}
+local i = tostring(game.PlaceId)
+if s[i] then loadstring(s[i])() end
