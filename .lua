6d4db438498c9b7d62a2652fc738dc86b6bacb8e
AST <elastraepicgames@gmail.com>
2026-07-01 22:44:46 +0200

    Update .lua

--- .lua
+++ .lua
@@ -1,6 +1,7 @@
 local s = {
     ["73814003954154"] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/astral-123/Pickaxe-Tycoon/refs/heads/main/.lua'))()",
     ["13822562292"] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/astral-123/midnight-chasser/refs/heads/main/.lua'))()",
+    ["13110267312"] = "loadstring(game:HttpGet("https://raw.githubusercontent.com/astral-123/AU/refs/heads/main/.lua"))()",
 }
 local i = tostring(game.PlaceId)
 if s[i] then
